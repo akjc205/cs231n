26733879f9a9e17c27c8ba1da2d6f49fa93f7670
akjc205 <104449438+akjc205@users.noreply.github.com>
2022-10-28 21:55:18 +0900

    3rd commit

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -33,18 +33,39 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+ # initialize the gradient as zero
 
-    pass
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
 
+    for i in range(num_train):
+      scores = X[i].dot(W)
+      scores -= np.max(scores)
+      scores_exp = np.sum(np.exp(scores))
+      correct_exp = np.exp(scores[y[i]])
+    
+      
+      loss -= np.log(correct_exp / scores_exp)
+
+      for j in range(num_classes):
+        if j == y[i]:
+          continue
+        dW[:, j] += X[i]/scores_exp * np.exp(scores[j])         
+      dW[:, y[i]] += (X[i]/scores_exp*np.exp(scores[y[i]]) - X[i])
+
+    loss /= num_train
+    dW /= num_train 
+
+    loss += reg * np.sum(W*W)
+    dW += 2*reg*W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
 
-
 def softmax_loss_vectorized(W, X, y, reg):
     """
     Softmax loss function, vectorized version.
-
     Inputs and outputs are the same as softmax_loss_naive.
     """
     # Initialize the loss and gradient to zero.
@@ -58,8 +79,23 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
 
-    pass
+    scores = X.dot(W)
+    scores -= np.max(scores)
+    scores_exp =  np.sum(np.exp(scores), axis=1) # 500*1
+    correct_scores_exp = np.exp(scores[np.arange(num_train), y])
+
+    loss = correct_scores_exp / scores_exp
+    loss = - np.sum(np.log(loss)/num_train + reg*np.sum(W*W))
+    s = np.divide(np.exp(scores), scores_exp.reshape(num_train, 1))
+    s[range(num_train), y] = 0
+    s[range(num_train), y] = -(scores_exp - correct_scores_exp) / scores_exp
+    dW = X.T.dot(s)
+    dW /= num_train
+    dW += 2*reg*W
+    
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
